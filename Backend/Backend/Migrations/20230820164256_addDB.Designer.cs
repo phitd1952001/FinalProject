cb54d82d9b8b5c193f7b01f42e0f0b70eba96842
Duong Phi <phitdgcs190826@fpt.edu.vn>
2023-08-21 21:30:21 +0700

    add

--- Backend/Backend/Migrations/20230820164256_addDB.Designer.cs
+++ Backend/Backend/Migrations/20230820164256_addDB.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230809081403_initdb")]
-    partial class initdb
+    [Migration("20230820164256_addDB")]
+    partial class addDB
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
